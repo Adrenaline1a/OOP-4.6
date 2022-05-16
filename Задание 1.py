ed14aa8e6140ca49ab3ce215a3b47bdff687d15d
Adrenaline1a <78856293+Adrenaline1a@users.noreply.github.com>
2022-05-16 11:23:57 +0300

    Аннотация переменных в функциях

--- Задание 1.py
+++ Задание 1.py
@@ -20,7 +20,7 @@ class Flight:
 class Listing:
     flights: List[Flight] = field(default_factory=lambda: [])
 
-    def adding(self, stay, number, value) -> None:
+    def adding(self, stay:str, number:str, value:str) -> None:
         self.flights.append(
             Flight(
                 stay=stay,
@@ -60,7 +60,7 @@ class Listing:
         table.append(line)
         return '\n'.join(table)
 
-    def selecting(self, nom) -> List[str]:
+    def selecting(self, nom:str) -> List[str]:
         """Выбор рейсов по типу самолёта"""
         count = 0
         result = []
@@ -78,7 +78,7 @@ class Listing:
 
         return result
         
-    def load(self, filename) -> str:
+    def load(self, filename:str) -> str:
         with open(filename, 'r', encoding='utf8') as fin:
             xml = fin.read()
         parser = ET.XMLParser(encoding="utf8")
@@ -103,7 +103,7 @@ class Listing:
                     )
         return filename
 
-    def save(self, filename) -> None:
+    def save(self, filename:str) -> None:
         root = ET.Element('flights')
         for flight in self.flights:
             flights_element = ET.Element('flights')
@@ -120,7 +120,7 @@ class Listing:
             tree.write(fout, encoding='utf8', xml_declaration=True)
 
 
-def main(command_line=None) -> None:
+def main(command_line:None=None) -> None:
     staff = Listing()
     file_parser = argparse.ArgumentParser(add_help=False)
     file_parser.add_argument(
